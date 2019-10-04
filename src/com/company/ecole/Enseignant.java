5ad1114b94a4aa9d9fc905a2079e97f32c786546
Maxime <maxime.schlegel.83@gmail.com>
2019-10-04 15:27:37 +0200

    Create hastables to store income and work hour

--- src/com/company/ecole/Enseignant.java
+++ src/com/company/ecole/Enseignant.java
@@ -2,29 +2,34 @@ package com.company.ecole;
 
 import com.company.personnes.NumeroSecuriteSocialeInvalideException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Enseignant extends Personnel {
     private String echelon;
-    private int service;
-    private int prime;
     private boolean serviceFait;
     private int heuresSupp;
+    private int mission;
+
+    private static int prime = 500;
+    private static Map<String, Integer> fixe = new HashMap<>(){{
+        //add the elements to the created hastable
+        put("PROF", 1000);
+        put("MDC", 2000);
+        put("PRAG", 4000);
+    }};
+    private static final Map<String, Integer> service = new HashMap<>() {{
+        //add the elements to the created hastable
+        put("PROF", 192/2);
+        put("MDC", 192);
+        put("PRAG", 192*2);
+    }};
 
     public Enseignant(String nom, String prenom, String numSecu, String date_entree, int temps_travail, String echelon) throws NumeroSecuriteSocialeInvalideException, TempsDeTravailException, EchelonException {
         super(nom, prenom, numSecu, date_entree, temps_travail);
 
-        switch (echelon) {
-            case "MDC":
-                service = 192;
-                break;
-            case "PROF":
-                service = 96;
-                break;
-            case "PRAG":
-                service = 192*2;
-                break;
-            default:
-                throw new EchelonException();
-        }
+        if (!fixe.containsKey(echelon))
+            throw new EchelonException();
 
         this.echelon = echelon;
     }
@@ -32,24 +37,14 @@ public class Enseignant extends Personnel {
     public Enseignant(String nom, String prenom, String numSecu, String adresse, String date_entree, int temps_travail, String echelon) throws NumeroSecuriteSocialeInvalideException, TempsDeTravailException, EchelonException {
         super(nom, prenom, numSecu, adresse, date_entree, temps_travail);
 
-        switch (echelon) {
-            case "MDC":
-                service = 192;
-                break;
-            case "PROF":
-                service = 96;
-                break;
-            case "PRAG":
-                service = 192*2;
-                break;
-            default:
-                throw new EchelonException();
-        }
+        if (!fixe.containsKey(echelon))
+            throw new EchelonException();
 
         this.echelon = echelon;
     }
 
     public boolean isServiceFait() {
+        //Doit pouvoir être calculer avec le temps réalisé + mission et le temps target
         return serviceFait;
     }
 
@@ -65,9 +60,18 @@ public class Enseignant extends Personnel {
         this.heuresSupp = heuresSupp;
     }
 
+    public int getMission() {
+        return mission;
+    }
+
+    public void setMission(int mission) {
+        this.mission = mission;
+    }
+
     @Override
     int calculSalaire() {
-        int somme = 0;
+        int somme = fixe.get(echelon);
+
         if(serviceFait)
             somme += prime;
 
