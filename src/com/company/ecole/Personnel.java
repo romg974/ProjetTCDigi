ce0fb03c6dd666fc93151f047a4edf62bd7c718a
Maxime <maxime.schlegel.83@gmail.com>
2019-10-04 13:42:47 +0200

    Add Person inheritance

--- src/com/company/ecole/Personnel.java
+++ src/com/company/ecole/Personnel.java
@@ -1,10 +1,34 @@
 package com.company.ecole;
 
-public abstract class Personnel {
+import com.company.personnes.NumeroSecuriteSocialeInvalideException;
+import com.company.personnes.Personne;
+
+public abstract class Personnel extends Personne {
+
     protected String date_entree;
     protected int temps_travail;
 
     static public int taux = 10;
+
+    public Personnel(String nom, String prenom, String numSecu, String date_entree, int temps_travail) throws NumeroSecuriteSocialeInvalideException, TempsDeTravailException {
+        super(nom, prenom, numSecu);
+
+        if (temps_travail < 0 || 100 < temps_travail)
+            throw new TempsDeTravailException();
+
+        this.date_entree = date_entree;
+        this.temps_travail = temps_travail;
+    }
+
+    public Personnel(String nom, String prenom, String numSecu, String adresse, String date_entree, int temps_travail) throws NumeroSecuriteSocialeInvalideException, TempsDeTravailException {
+        super(nom, prenom, numSecu, adresse);
+
+        if (temps_travail < 0 || 100 < temps_travail)
+            throw new TempsDeTravailException();
+
+        this.date_entree = date_entree;
+        this.temps_travail = temps_travail;
+    }
 
     abstract int calculSalaire();
 }
