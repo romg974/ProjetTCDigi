038af03ae09344cf583f56041ee116cd18cdfab7
Maxime <maxime.schlegel.83@gmail.com>
2019-10-04 13:53:31 +0200

    Rework of the Constructor

--- src/com/company/ecole/Enseignant.java
+++ src/com/company/ecole/Enseignant.java
@@ -1,5 +1,7 @@
 package com.company.ecole;
 
+import com.company.personnes.NumeroSecuriteSocialeInvalideException;
+
 public class Enseignant extends Personnel {
     private String echelon;
     private int service;
@@ -7,15 +9,44 @@ public class Enseignant extends Personnel {
     private boolean serviceFait;
     private int heuresSupp;
 
-    public Enseignant(String echelon) {
-        this.echelon = echelon;
+    public Enseignant(String nom, String prenom, String numSecu, String date_entree, int temps_travail, String echelon) throws NumeroSecuriteSocialeInvalideException, TempsDeTravailException, EchelonException {
+        super(nom, prenom, numSecu, date_entree, temps_travail);
 
-        if(echelon == "MDC")
-            service = 192;
-        else if(echelon == "PROF")
-            service = 96;
-        else
-            service = 192*2;
+        switch (echelon) {
+            case "MDC":
+                service = 192;
+                break;
+            case "PROF":
+                service = 96;
+                break;
+            case "PRAG":
+                service = 192*2;
+                break;
+            default:
+                throw new EchelonException();
+        }
+
+        this.echelon = echelon;
+    }
+
+    public Enseignant(String nom, String prenom, String numSecu, String adresse, String date_entree, int temps_travail, String echelon) throws NumeroSecuriteSocialeInvalideException, TempsDeTravailException, EchelonException {
+        super(nom, prenom, numSecu, adresse, date_entree, temps_travail);
+
+        switch (echelon) {
+            case "MDC":
+                service = 192;
+                break;
+            case "PROF":
+                service = 96;
+                break;
+            case "PRAG":
+                service = 192*2;
+                break;
+            default:
+                throw new EchelonException();
+        }
+
+        this.echelon = echelon;
     }
 
     public boolean isServiceFait() {
